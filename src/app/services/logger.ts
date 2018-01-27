486d56aefb534e81602f14c6570fe4114bd3cc6d
Niten <niten2@yandex.ru>
2018-01-27 23:02:38 +0700

    commit - Сб янв 27 23:02:38 +07 2018

--- src/app/services/logger.ts
+++ src/app/services/logger.ts
@@ -1,42 +1,7 @@
 import settings from "config/settings"
 import * as intel from "intel"
 
-// declare interface LoggerType {
-//   // (source: string, subString: string): boolean;
-
-
-//   // info(header: string): any;
-
-//   info: (content: string) => void;
-//   error: (content: string) => void;
-
-//   // info: any
-//   // error: any
-//   // info(content: string)
-//   // error(content: string)
-
-//   // info(content: string): void
-//   // error(content: string): void
-// //   info: () =>
-// //   error: string
-// }
-
-// interface Logger {
-//   info: (content: string) => void;
-//   error: (content: string) => void;
-// }
-
-
-
 const buildLogger = (): any => {
-  // const logger: Logger = {
-  //   info: (content: string): void => {},
-  //   error: (content: string): void => {},
-  // }
-
-  // return logger
-  // return params
-
   if (settings.isEnvTest) {
     return {
       info: (context: string): void => {},
@@ -47,10 +12,6 @@ const buildLogger = (): any => {
   return intel
 }
 
-// const res: LoggerType = logger()
-
 const logger = buildLogger()
 
-// export const logger = buildLogger()
 export default logger
-// export default res
